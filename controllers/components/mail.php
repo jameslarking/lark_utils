0a199161fcb7d46bb7018f80beb9a445831603b3
James Larking <jim@citadelsecure.com>
2011-04-04 12:58:15 +0100

    send by aws ses

--- controllers/components/mail.php
+++ controllers/components/mail.php
@@ -8,6 +8,37 @@ class MailComponent extends EmailComponent{
 	function startup(){
 		$this->from=Configure::read("Site.name")." <".Configure::read("Site.email").">";
 		$this->sendAs="both";
-	}	
+	}
+    
+   function _aws_ses() { 
+      require_once 'AWSSDKforPHP/sdk.class.php';
+      $ses = new AmazonSES(); 
+      $destination = array( 
+         'ToAddresses' => explode(',', $this->to) 
+      ); 
+      $message = array( 
+         'Subject' => array( 
+            'Data' => $this->subject 
+         ), 
+         'Body' => array() 
+      ); 
+      if($this->textMessage != NULL) { 
+         $message['Body']['Text'] = array( 
+            'Data' => $this->textMessage 
+         ); 
+      } 
+      if($this->htmlMessage != NULL) { 
+         $message['Body']['Html'] = array( 
+            'Data' => $this->htmlMessage 
+         ); 
+      } 
+       
+      $response = $ses->send_email($this->from, $destination, $message); 
+      $ok = $response->isOK(); 
+      if(!$ok) { 
+         $this->log('Error sending email from AWS SES: '.json_encode($response->body). " Message was: ".$response->header['x-aws-body'], 'error'); 
+      } 
+      return $ok; 
+   } 
 }
 ?>
