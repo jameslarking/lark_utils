a4ddc1a5a223933fd98998e1f0ff6c50bb590f73
James Larking <jim@citadelsecure.com>
2011-04-16 16:57:35 +0100

    sitemate upload

--- controllers/components/sitemate.php
+++ controllers/components/sitemate.php
@@ -2,15 +2,17 @@
 class SitemateComponent extends Object{
 	
 	function store($file,$path="",$meta=array()){
+		set_time_limit(0);
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, Configure::read("sitemate.posturl"));
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+		$post=array(
 			"file"=>"@".$file,
 			"apikey"=>Configure::read("sitemate.apikey"),
 			"path"=>$path,
 			"meta"=>json_encode($meta)
-		)); 
+		);
+		curl_setopt($ch, CURLOPT_URL, Configure::read("sitemate.posturl"));
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$return=curl_exec($ch);
 		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
